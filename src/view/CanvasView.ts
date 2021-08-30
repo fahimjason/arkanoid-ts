4ac215e7654d48b3f5109f09973246f8b03b2c01
JisanFahim <JisanFahim@users.noreply.github.com>
2021-08-31 00:48:36 +0600

    Project Setup done

---
+++ src/view/CanvasView.ts
@@ -0,0 +1,9 @@
+import {Ball} from '../sprites/Ball';
+
+export class CanvasView {
+    private canvas: HTMLCanvasElement;
+    private context: CanvasRenderingContext2D | null;
+    private scoreDisplay: HTMLObjectElement | null;
+    private start: HTMLObjectElement | null;
+    private info: HTMLObjectElement | null;
+}
