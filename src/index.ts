3f57c172592824462bd8edaf471dca55b6b04dfd
JisanFahim <JisanFahim@users.noreply.github.com>
2021-09-01 20:29:25 +0600

    scaffold index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,40 @@
-// Start here
-console.log("Start here!");
+import { CanvasView } from './view/CanvasView';
+import { Brick } from './sprites/Brick';
+import { Paddle } from './sprites/Paddle';
+import { Ball } from './sprites/Ball';
+// Images
+import PADDLE_IMAGE from './images/paddle.png';
+import BALL_IMAGE from './images/ball.png';
+// Level & colors
+import {
+    PADDLE_SPEED,
+    PADDLE_WIDTH,
+    PADDLE_HEIGHT,
+    PADDLE_STARTX,
+    BALL_SPEED,
+    BALL_SIZE,
+    BALL_STARTY,
+    BALL_STARTX
+} from './setup';
+
+let gameOver = false;
+let score = 0;
+
+function setGameOver(view: CanvasView) {
+    view.drawInfo('Game Over!');
+    gameOver = false;
+}
+
+function setGameWin(
+    view: CanvasView,
+    bricks: Brick[],
+    paddle: Paddle,
+    ball: Ball
+) {}
+
+function startGame(view: CanvasView) {}
+
+// Create a new view for the game
+const view = new CanvasView('#playField');
+view.initStartButton(startGame);
+
