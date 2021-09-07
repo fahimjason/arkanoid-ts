ab5291f5e6f1b25ae0ef74ba909d80f1035f1892
JisanFahim <JisanFahim@users.noreply.github.com>
2021-09-07 12:24:19 +0600

    added helper function and draw bricks fuctionalities

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,61 @@
-import { CanvasView } from './view/CanvasView';
-import { Brick } from './sprites/Brick';
-import { Paddle } from './sprites/Paddle';
-import { Ball } from './sprites/Ball';
+import { CanvasView } from "./view/CanvasView";
+import { Brick } from "./sprites/Brick";
+import { Paddle } from "./sprites/Paddle";
+import { Ball } from "./sprites/Ball";
 // Images
-import PADDLE_IMAGE from './images/paddle.png';
-import BALL_IMAGE from './images/ball.png';
+import PADDLE_IMAGE from "./images/paddle.png";
+import BALL_IMAGE from "./images/ball.png";
 // Level & colors
 import {
-    PADDLE_SPEED,
-    PADDLE_WIDTH,
-    PADDLE_HEIGHT,
-    PADDLE_STARTX,
-    BALL_SPEED,
-    BALL_SIZE,
-    BALL_STARTY,
-    BALL_STARTX
-} from './setup';
+  PADDLE_SPEED,
+  PADDLE_WIDTH,
+  PADDLE_HEIGHT,
+  PADDLE_STARTX,
+  BALL_SPEED,
+  BALL_SIZE,
+  BALL_STARTY,
+  BALL_STARTX,
+} from "./setup";
+import { createBricks } from "./helpers";
 
 let gameOver = false;
 let score = 0;
 
 function setGameOver(view: CanvasView) {
-    view.drawInfo('Game Over!');
-    gameOver = false;
+  view.drawInfo("Game Over!");
+  gameOver = false;
 }
 
 function setGameWin(
-    view: CanvasView,
-    bricks: Brick[],
-    paddle: Paddle,
-    ball: Ball
+  view: CanvasView,
+  bricks: Brick[],
+  paddle: Paddle,
+  ball: Ball
 ) {}
 
-function startGame(view: CanvasView) {}
+function gameLoop(
+  view: CanvasView,
+  bricks: Brick[]
+  // paddle: Paddle,
+  // ball: Ball
+) {
+  view.clear();
+  view.drawBricks(bricks);
+
+  requestAnimationFrame(() => gameLoop(view, bricks));
+}
+
+function startGame(view: CanvasView) {
+  // Reset displays
+  score = 0;
+  view.drawInfo("");
+  view.drawScore(0);
+  // Create bricks
+  const bricks = createBricks();
+
+  gameLoop(view, bricks);
+}
 
 // Create a new view for the game
-const view = new CanvasView('#playField');
+const view = new CanvasView("#playField");
 view.initStartButton(startGame);
-
